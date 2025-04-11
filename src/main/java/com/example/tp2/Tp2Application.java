992d97fdc2178fd814a257e2106b97602d13f0a8
Mariem <mariem.landoulsi2002@gmail.com>
2025-04-11 09:03:38 +0100

    le premier commit

---
+++ src/main/java/com/example/tp2/Tp2Application.java
@@ -0,0 +1,14 @@
+package com.example.tp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Application.class, args);
+		System.out.println("It is a continuous integration job";)
+	}
+
+}
---
+++ src/test/java/com/example/tp2/Tp2ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.tp2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("it is a test class");
+		assertEquals(true,true);
+	}
+
+}
